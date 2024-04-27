97e1f9d838793ed3cbef2e9db649729365e7df9e
ubuntu <piotr.perz@wp.pl>
2024-04-28 00:28:10 +0200

    Day 26: Animals finished

--- animals.py
+++ animals.py
@@ -5,11 +5,15 @@ from pathlib import Path
 
 filepaths = glob.glob("animals/*")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_font(family="Times", size=16, style="B")
 for path in filepaths:
+    with open(path, "r") as file:
+        content = file.read()
     pdf.add_page()
     filename = Path(path).stem
-    pdf.cell(w=50, h=8, txt=f"{filename.title()}")
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"{filename.title()}", ln=1)
+    pdf.set_font(family="Times", size=10)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 
 pdf.output(f"PDFs/animals.pdf")
