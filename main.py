8fdd65505d6117e18ccc42ab0f681d12b841304e
ubuntu <piotr.perz@wp.pl>
2024-04-27 23:52:25 +0200

    Day 26: Add header, total price, company and logo

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=columns[2], border=1, align="C")
     pdf.cell(w=30, h=8, txt=columns[3], border=1, align="C")
     pdf.cell(w=30, h=8, txt=columns[4], border=1, align="C", ln=1)
-
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
@@ -37,5 +36,22 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1, align="C")
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align="C")
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, align="C", ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, border=1, align="C")
+    pdf.cell(w=70, h=8, border=1, align="C")
+    pdf.cell(w=30, h=8, border=1, align="C")
+    pdf.cell(w=30, h=8, border=1, align="C")
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, align="C", ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", align="L", ln=1)
+
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
 
